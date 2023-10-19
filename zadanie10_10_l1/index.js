3609275812e5c8d931d2a88d6d1493415c58deb7
Piatekf <filip.piatek2005@gmail.com>
2023-10-19 10:56:44 +0200

    z10_10_l1 function writeNumbersToFIle bug fixed

--- zadanie10_10_l1/index.js
+++ zadanie10_10_l1/index.js
@@ -1,21 +1,20 @@
 const fs = require('fs');
 
-function* generateNumbersGenerator() {
+function* generateNumbers() {
   for (let i = 0; i < 20; i++) {
     yield Math.floor(Math.random() * (2137 - (-420) + 1)) - 420;
   }
 }
 
 function writeNumbersToFile() {
-  const numbers = generateNumbers();
   const timestamp = new Date().toISOString().replace(/:/g, '-');
   const filename = `random-${timestamp}.txt`;
 
   const writeStream = fs.createWriteStream(filename);
 
-  numbers.forEach(number => {
+  for (const number of generateNumbers()) {
     writeStream.write(number + '\n');
-  });
+  }
 
   writeStream.end();
 }
