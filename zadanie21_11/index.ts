d94faab47b7cbdee4cb5ab4a387faf88bef52e70
Piatekf <filip.piatek2005@gmail.com>
2023-11-22 17:17:31 +0100

    imported all routes

--- zadanie21_11/index.ts
+++ zadanie21_11/index.ts
@@ -2,6 +2,10 @@ import express, {Express} from 'express';
 import { PrismaClient } from '@prisma/client';
 import bodyParser from 'body-parser';
 import usersRouter from './routes/user.routes';
+import postsRoutes from './routes/posts.routes';
+import commentsRoutes from './routes/comments.routes';
+import categoriesRoutes from './routes/categories.routes';
+import profilesRoutes from './routes/profiles.routes';
 
 const prisma = new PrismaClient();
 const app: Express = express();
