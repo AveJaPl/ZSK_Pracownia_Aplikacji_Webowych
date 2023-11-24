b2c5ccd02af330705e99884e94334b32a8252920
Piatekf <filip.piatek2005@gmail.com>
2023-11-25 00:11:46 +0100

    added fetching by authorid and post/comment id

--- zadanie21_11/routes/comments.routes.ts
+++ zadanie21_11/routes/comments.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getCommentsController, getCommentController, createCommentController, updateCommentController, deleteCommentController } from '../controllers/comments.controller';
+import { getCommentsController, getCommentController, createCommentController, updateCommentController, deleteCommentController, getCommentsByAuthorController } from '../controllers/comments.controller';
 
 const router = express.Router();
 
 router.get('/', getCommentsController);
+router.get('/author/:id', getCommentsByAuthorController);
 router.post('/', createCommentController);
 router.put('/:id', updateCommentController);
 router.delete('/:id', deleteCommentController);
--- zadanie21_11/routes/posts.routes.ts
+++ zadanie21_11/routes/posts.routes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getPostsController, getPostController, createPostController, updatePostController, deletePostController } from '../controllers/posts.controller';
-
+import { getPostsController, getPostController, createPostController, updatePostController, deletePostController, getPostsByAuthorController } from '../controllers/posts.controller';
 const router = express.Router();
 
 router.get('/', getPostsController);
+router.get('/author/:id', getPostsByAuthorController);
 router.post('/', createPostController);
 router.put('/:id', updatePostController);
 router.delete('/:id', deletePostController);
