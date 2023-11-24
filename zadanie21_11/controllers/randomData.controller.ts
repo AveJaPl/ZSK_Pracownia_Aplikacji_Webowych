c15c8849a0897813b6c45102390f24fd317ac89e
Piatekf <filip.piatek2005@gmail.com>
2023-11-25 00:10:56 +0100

    randomData logging

--- zadanie21_11/controllers/randomData.controller.ts
+++ zadanie21_11/controllers/randomData.controller.ts
@@ -1,10 +1,13 @@
 import { Response, Request } from "express";
 import { randDataModel } from "../data.schema";
+import { connectToDatabase } from "../db.config";
 
 const createRandomDataController = async (req: Request, res: Response) => {
     try {
+        await connectToDatabase()
         const { key, value } = req.body;
-        const newData = new randDataModel({ key: key, value: value });
+        const newData = new randDataModel({ key, value });
+        console.log(newData);
         const createdData = await newData.save();
         console.log(createdData);
         res.status(201).json(createdData);
